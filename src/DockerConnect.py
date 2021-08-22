423b4a84a3c92faba382e72c19bae239e2c5d805
nstoltzfus <nstoltzfus3@gatech.edu>
2021-08-21 23:50:55 -0700

    successful connection of docker container startup combined with psycopg connection feature commit

--- src/DockerConnect.py
+++ src/DockerConnect.py
@@ -5,6 +5,9 @@ import docker
 import subprocess
 import os
 import time
+import requests
+
+from PSQLServer import PSQLServer
 
 
 class DockerConnect:
@@ -20,17 +23,24 @@ class DockerConnect:
 
         bashCommand = "docker run -d -p %d:5432 %s"
 
+        self.dbs = {}
         for line in open(inifile):
             # starts containers while distributing monotonically increasing ports.
             # Opted to bash launch the containers then scrape the containers off the daemon
             # Made because original documentation was given for bash.
 
             # client.containers.run(line, ports={'%d/tcp' % port : 5432}, detach=True)
-            process = subprocess.run((bashCommand % (port, line)).split())
+            singlePSQLiniLine = line.strip().split(',')
+            user = singlePSQLiniLine[0]
+            dbname = singlePSQLiniLine[1]
+            password = singlePSQLiniLine[2]
+            self.dbs[singlePSQLiniLine[3]] = PSQLServer(user, dbname, password, port)
+            process = subprocess.run((bashCommand % (port, line.split(',')[3])).split())
             port += 1
 
-        print(client.containers.list())
         self.containers = client.containers.list()
+
+
 
     def closeAll(self):
         for container in self.containers:
@@ -47,6 +57,31 @@ class DockerConnectTest:
 
 if __name__ == "__main__":
     dc = DockerConnect("init.txt")
+
+    psqlConnections = []
+    for container in dc.containers[::-1]:
+        try:
+            PSQLdb = dc.dbs[container.image.attrs['RepoTags'][0]]
+            print("Database: %s running on port %d" % (PSQLdb.dbname, PSQLdb.port))
+
+            conn = psycopg2.connect(
+                host="localhost",
+                database=PSQLdb.dbname,
+                user=PSQLdb.user,
+                password=PSQLdb.password,
+                port=PSQLdb.port
+                )
+            psqlConnections.append(conn)
+            print("Connection established...")
+        except Exception as e:
+            print(e)
+
+    for connection in psqlConnections:
+        cur = connection.cursor()
+        cur.execute("select relname from pg_class where relkind='r' and relname !~ '^(pg_|sql_)';")
+        print(cur.fetchall())
+
+    dc.closeAll()
 
 
 
--- src/PSQLServer.py
+++ src/PSQLServer.py
@@ -2,8 +2,9 @@
 
 class PSQLServer:
 
-    def __init__(self, user, dbname, password):
+    def __init__(self, user, dbname, password, port):
         self.user = user
         self.dbname = dbname
         self.password = password
+        self.port = port
 
